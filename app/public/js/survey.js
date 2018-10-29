7fb13b65ed5b6ef642cf2a196c038e089abc6e23
mbennett <mbennett5516@yahoo.com>
2018-10-28 19:13:30 -0500

    finishing touches

--- app/public/js/survey.js
+++ app/public/js/survey.js
@@ -1,11 +1,26 @@
-const getEmpObj = function () {
-    let employee = {
-        name: $('#name').val(),
-        photo: $('#photo').val(),
-        scores: [$('#q1').val(), $('#q2').val(), $('#q3').val(), $('#q4').val(), $('#q5').val(), $('#q6').val(), $('#q7').val(), $('#q8').val(), $('#q9').val(), $('#q10').val(),]
-    }
-    return employee;
+const validate = function(){
+    let valid = true;
+    $('input').each(function(){
+        if(!$(this).val())
+        valid = false;
+    });
+
+    $('.custom-select').each(function(){
+        if(!$(this).val())
+        valid = false;
+    })
+    return valid;
 }
+
+const getEmpObj = function () {
+        let employee = {
+            name: $('#name').val(),
+            photo: $('#photo').val(),
+            scores: [$('#q1').val(), $('#q2').val(), $('#q3').val(), $('#q4').val(), $('#q5').val(), $('#q6').val(), $('#q7').val(), $('#q8').val(), $('#q9').val(), $('#q10').val(),]
+        }
+        return employee;    
+    }
+
 
 const submitSurvey = function (employee) {
     console.log(employee);
@@ -17,13 +32,17 @@ const submitSurvey = function (employee) {
         $('#match-name').text(response.name);
         console.log(response.photo);
         $('#match-img').attr("src", response.photo);
+        $('#results-modal').modal('toggle');
     });
 }
 
 const run = function (event) {
     event.preventDefault();
     const employee = getEmpObj();
+    if (validate())
     submitSurvey(employee);
+    else
+    alert("Please Fill out all fields");
 }
 
 $('#submit').on('click', run);
